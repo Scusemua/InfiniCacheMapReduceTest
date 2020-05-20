ba27100948d04f73263fcc6015f6d4e0f013f7d5
Scusemua <benrcarver@gmail.com>
2020-05-20 14:45:27 -0400

    Pass nReducers as command-line argument to start-client.sh

--- main/start-client.sh
+++ main/start-client.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 DRIVER_HOST="ec2-18-212-213-199.compute-1.amazonaws.com:1234"
 
-# The first command-line argument should be the file of S3 keys. 
+# The first command-line argument is nReducers.
+# The second command-line argument should be the file of S3 keys. 
+
+# Examples for second argument:
 # /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt
 # /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/5GB_S3Keys.txt
 
-go run client.go $DRIVER_HOST srt 10 sample_data.dat $1 &
+go run client.go $DRIVER_HOST srt $1 sample_data.dat $2 &
 pids[0]=$!
 
 echo "[Test]: waiting for client and workers to finish..." > /dev/stderr
