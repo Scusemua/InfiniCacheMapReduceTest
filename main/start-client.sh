d21de4aed68a6497bf8daced29d1e5be14e5a595
Scusemua <benrcarver@gmail.com>
2020-05-20 14:33:17 -0400

    Modified start-client script to take argument for S3 keys file

--- main/start-client.sh
+++ main/start-client.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 DRIVER_HOST="ec2-18-212-213-199.compute-1.amazonaws.com:1234"
 
-go run client.go $DRIVER_HOST srt 10 sample_data.dat /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt &
+# The first command-line argument should be the file of S3 keys. 
+# /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt
+# /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/5GB_S3Keys.txt
+
+go run client.go $DRIVER_HOST srt 10 sample_data.dat $1 &
 pids[0]=$!
 
 echo "[Test]: waiting for client and workers to finish..." > /dev/stderr
