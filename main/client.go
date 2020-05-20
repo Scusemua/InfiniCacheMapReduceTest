d69f3279e93bc7f4c41a37ffb4c903bb416ce018
Scusemua <benrcarver@gmail.com>
2020-05-20 19:41:29 -0400

    Print os.Args in client

--- main/client.go
+++ main/client.go
@@ -28,6 +28,7 @@ import (
 // wc is the job name,
 // and pg-*.txt are the input files to pass to the worker(s).
 func main() {
+	fmt.Println("os.Args =", os.Args)
 	drv := serverless.NewDriver(os.Args[1]) // the 1st cmd-line argument: driver hostname and ip addr
 	jobName := os.Args[2]                   // the 2nd cmd-line argument: MapReduce job name
 	nReduce, err := strconv.Atoi(os.Args[3])
