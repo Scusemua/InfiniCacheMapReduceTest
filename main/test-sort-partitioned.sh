f02fd9d27dda33545c25b3ab66a1879efa65921e
Scusemua <benrcarver@gmail.com>
2020-11-28 12:52:45 -0500

    Added some comments to 'test-sort-partitioned.sh', fixed syntax error hopefully for real in 'start-workers.sh'

--- main/test-sort-partitioned.sh
+++ main/test-sort-partitioned.sh
@@ -3,25 +3,37 @@ DATE=`date "+%Y%m%d%H%M"`
 
 START=`date +"%Y-%m-%d %H:%M:%S"`
 
-go run client.go -driverHostname localhost:1234 -jobName srt -nReduce 10 -sampleDataKey sample_data.dat -s3KeyFile /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt -dataShards 10 -parityShards 2 -maxGoRoutines 32 -storageIps "127.0.0.1:6378"
+echo "Launching MapReduce client."
 
+go run client.go -driverHostname localhost:1234 -jobName srt -nReduce 10 -sampleDataKey sample_data.dat -s3KeyFile /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt -dataShards 10 -parityShards 2 -maxGoRoutines 32 -storageIps "127.0.0.1:6378"
 #go run client.go localhost:1234 srt 10 sample_data.dat /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt 10 2 32 &
 pids[0]=$!
+
+echo "Client launched."
+echo "Launching worker #1."
 
 go run worker.go localhost:1235 localhost:1234 999999 & 
 pids[1]=$!
 
+echo "Launching worker #2."
+
 go run worker.go localhost:1236 localhost:1234 999999 & 
 pids[2]=$!
+
+echo "Launching worker #3."
 
 go run worker.go localhost:1237 localhost:1234 999999 & 
 pids[3]=$!
 
-# go run worker.go localhost:1238 localhost:1234 999999 & 
-# pids[4]=$!
+echo "Launching worker #4."
 
-# go run worker.go localhost:1239 localhost:1234 999999 & 
-# pids[5]=$!vals
+go run worker.go localhost:1238 localhost:1234 999999 & 
+pids[4]=$!
+
+echo "Launching worker #5."
+
+go run worker.go localhost:1239 localhost:1234 999999 & 
+pids[5]=$!vals
 
 echo "[Test]: waiting for client and worker to finish..." > /dev/stderr
 for pid in ${pids[*]}; do
