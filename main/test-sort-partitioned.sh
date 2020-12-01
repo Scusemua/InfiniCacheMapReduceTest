cd4b1cb6092cc60d09b26a8b3323be94794a4af8
Scusemua <benrcarver@gmail.com>
2020-11-30 20:18:21 -0500

    Updated script

--- main/test-sort-partitioned.sh
+++ main/test-sort-partitioned.sh
@@ -14,7 +14,7 @@ if [ "$1" != "" ] ; then
   KEY_FILE = $1
 fi
 
-go run client.go -driverHostname 127.0.0.1:1234 -jobName srt -nReduce 10 -sampleDataKey sample_data.dat -s3KeyFile $KEY_FILE -dataShards 10 -parityShards 2 -maxGoRoutines 32 -storageIps "127.0.0.1:6378" & 
+go run client.go -driverHostname 127.0.0.1:1234 -jobName srt -nReduce 10 -sampleDataKey sample_data.dat -s3KeyFile "$KEY_FILE" -dataShards 10 -parityShards 2 -maxGoRoutines 32 -storageIps "127.0.0.1:6378" & 
 #go run client.go localhost:1234 srt 10 sample_data.dat /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt 10 2 32 &
 pids[0]=$!
 
