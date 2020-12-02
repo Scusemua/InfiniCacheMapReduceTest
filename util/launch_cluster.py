18a6bd65d7fde0b1530bbca3c3b3c603a0aa1226
Ben <benrcarver@gmail.com>
2020-12-02 12:03:50 -0500

    Intermediate checkin; updating documentation for launch_cluster.py.

--- util/launch_cluster.py
+++ util/launch_cluster.py
@@ -6,7 +6,38 @@ from datetime import datetime
 import redis 
 import launch_cluster as lc
 
+"""
+This file contains scripts that I use to orchestrate workloads on remote clusters/virtual machines.
+
+You could write scripts to automatically deploy and run everything by running a single function, but
+I typically write out the function calls with the parameters I intend to use, and then just copy-and-
+paste them into a terminal.
+
+At the very least, you need to modify the variable declarations at the top of the file to match
+your setup. 
+
+INFINISTORE_DIRECTORY is the path to the root of the InfiniStore GitHub repo. Note that this should
+be under your $GOPATH environment variable. (My GOPATH is /home/ubuntu/project, I believe). Use Google
+to learn about GOPATH.
+
+KEYFILE_PATH is the path to your SSH key so Paramiko can SSH onto your VM's to execute commands.
+"""
+
+# This is the path to the root of the InfiniStore local GitHub repository on the VM.
+INFINISTORE_DIRECTORY = "/home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/"
+
+# The path to your keyfile.
+KEYFILE_PATH = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"
+
 def get_private_ips(region_name="us-east-1"):
+    """
+    Get the private IPv4 addresses of EC2 instances.
+
+    Arguments:
+        - The AWS region in which your VMs are running.
+    Returns:
+        A list of strings, where each string is a private IPv4 of an EC2 VM.
+    """
     print("Getting public IPs now...")
     ec2client = boto3.client('ec2', region_name = region_name)
     response = ec2client.describe_instances()
@@ -22,6 +53,15 @@ def get_private_ips(region_name="us-east-1"):
     return private_ips
 
 def get_ips(region_name="us-east-1"):
+    """
+    Get the public AND private IPv4 addresses of EC2 instances.
+
+    Arguments:
+        - The AWS region in which your VMs are running.
+    Returns:
+        A tuple of two lists, where the first list is the list of
+        public IPs and the second list is the list of private IPs.
+    """
     print("Getting public and private IPs now...")
     ec2client = boto3.client('ec2', region_name = region_name)
     response = ec2client.describe_instances()
@@ -42,6 +82,14 @@ def get_ips(region_name="us-east-1"):
     return public_ips, private_ips
 
 def get_public_ips(region_name="us-east-1"):
+    """
+    Get the public IPv4 addresses of EC2 instances.
+
+    Arguments:
+        - The AWS region in which your VMs are running.
+    Returns:
+        A list of strings, where each string is a public IPv4 of an EC2 VM.
+    """    
     print("Getting public IPs now...")
     ec2client = boto3.client('ec2', region_name = region_name)
     response = ec2client.describe_instances()
@@ -61,8 +109,36 @@ def execute_command(
     count_limit = 3,
     get_pty = True,
     ips = None,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"
+    key_path = KEYFILE_PATH
 ):
+    """
+    Execute a command-line/terminal command on a remote VM.
+
+    Arguments:
+        command (str): The full command that you wish to execute.
+
+        count_limit (int): This is the maximum number of attempts we make to retrieve output
+                           from the VM after executing our command. Each attempt takes time, and
+                           sometimes you don't really care about the output, so you don't bother
+                           spending time trying to retrieve it (meaning you pass, like, 1 for this).
+
+        get_pty (bool): This corresponds to the Paramiko (SSH library) function argument of the
+                        same name. When this is true, it means that you are requesting a pseudo-
+                        terminal from the server. This is usually used right after creating a client
+                        channel, to ask the server to provide some basic terminal semantics for a shell
+                        invoked with invoke_shell. 
+
+                        Basically, this allows us to get output from the commands we execute. This 
+                        should generally be True.
+
+                        https://www.kite.com/python/docs/paramiko.Channel.get_pty
+
+        ips (list of strings): The IPs of all the VM's you wish to execute your command on.
+                               These should be public IP's, unless you're running this script
+                               from an EC2 VM, in which case you may be able to use private IP's.
+
+        key_path: The full path to your private key-file for SSH-ing into the VM.
+    """
     keyfile = paramiko.RSAKey.from_private_key_file(key_path)
     ssh_clients = list()
     print(" ")
@@ -102,12 +178,15 @@ def launch_redis_servers(
     connect_and_ping = True,
     count_limit = 3,
     ips = None,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem",
+    key_path = KEYFILE_PATH,
     kill_first = False ,
     shards_per_vm = 1,
     starting_port = 6379
 ):
     """
+    Launch Redis virtual machines on VM's. This assumes that the VM has been configured a certain
+    way so that redis-server is installed (I think you can install it by running either
+    "apt-get install redis-server" or "yum install redis-server" on your VM if it isn't already installed.)
     Returns: 
         list of str: hostnames of instances that were launched.
     """
@@ -151,12 +230,30 @@ def launch_redis_servers(
 
 def launch_client(
     client_ip = None,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem",
+    key_path = KEYFILE_PATH,
     nReducers = 10,
     #s3_key_file = "/home/ubuntu/project/src/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt"
     s3_key_file = "/home/ubuntu/project/src/github.com/Scusemua/InfiniCacheMapReduceTest/util/1MB_S3Keys.txt"
     # /home/ubuntu/project/src/InfiniCacheMapReduceTest/util/5GB_S3Keys.txt
 ):
+    """
+    Launch a MapReduce client on the given VM.
+
+    DOES NOT CURRENTLY WORK. I just manually launch the VM so I can see the output reliably and in
+    real time. This could be easily modified to work by editing the command passed to execute_command(),
+    as currently this command corresponds to an older version of the MapReduce framework.
+
+    Arguments:
+        client_ip (string): The IP address of the VM on which you wish to run a client.
+        key_path (string): The path to your keyfile for SSH-ing into the VM.
+        nReducers (int): This is a MapReduce argument needed by the client. This controls how many
+                         reducers there are. (In MapReduce, there are Mappers and Reducers. Use Google
+                         for more information on this topic.)
+        s3_key_file (String): The path to the text file containing the list of S3 keys, where each
+                              S3 key is for a chunk of input data. This file should be on the VM, and 
+                              thus this path should be for the client VM (not the computer on which
+                              you're running this script, unless they're the same VM, I guess).
+    """
     pre_command = """
     . ~/.profile;
     . ~/.bashrc;
@@ -180,6 +277,16 @@ def wondershape(
     upload_bytes = 1024000,
     download_bytes = 1024000
 ):
+    """
+    This assumes you have wondershaper installed on the target VM. Wondershaper is used to artificially
+    restrict the upload/download (Internet) speed of your VM for testing purposes.
+
+    Arguments:
+        ips (list of strings): The IPs of the VM's on which you want to run this function. 
+                               Should be public IPv4's unless you are running this script on an EC2 VM. 
+        upload_bytes (int): The desired upload speed in bytes.
+        download_bytes (int): The desired download speed in bytes.
+    """
     command = "sudo wondershaper -a eth0 -u {} -d {}".format(upload_bytes, download_bytes)
     execute_command(command = command, count_limit = 1, get_pty = True, ips = ips)
 
@@ -187,6 +294,18 @@ def ping_redis(
     ips = None,
     hostnames = None
 ):
+    """
+    This creates a Redis client for each of the specified VMs and executes the ping() function. Basically
+    used to check if they're running.
+
+    Arguments:
+    Only pass ONE of the two arguments to this function.
+        ips (list of str): These are the IPs of the VM with no ports. This assumes each VM only has one
+                           Redis instance listening on port 6379.
+        hostnames (list of str): List of strings of the form "<IP>:<PORT>" where each string corresponds
+                                 to a Redis instance running on the VM with IP "<IP>", listening on port
+                                 "<PORT>".
+    """
     if hostnames is not None:
         count = 1
         for hostname in hostnames:
@@ -215,10 +334,23 @@ def update_redis_hosts(
     hostnames = None,
     ips = None,
     redis_ips = None,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"
+    key_path = KEYFILE_PATH
 ):
     """
-    Update the redis_hosts.txt file on the given VMs.
+    Update the redis_hosts.txt file on the given VMs. Previously this file was used by the MapReduce
+    framework, as the framework would read in all the hostnames from the file and use this to create
+    Redis clients for each host. This feature is not currently used.
+
+    Arguments:
+        ips (list of str): The IPv4's of the VM's on which you want to update the redis host file.
+        redis_ips (list of str): These are the IPs of the VM with no ports. This assumes each VM only 
+                                 has one Redis instance listening on port 6379.
+        hostnames (list of str): List of strings of the form "<IP>:<PORT>" where each string corresponds
+                                 to a Redis instance running on the VM with IP "<IP>", listening on port
+                                 "<PORT>".
+        key_path (string): The path to your keyfile for SSH-ing into the VM.
+
+        You only need to pass one of redis_ips and hostnames, not both.
     """
     print("Key path = {}".format(key_path))
     create_redis_file_command = "printf \""
@@ -249,25 +381,28 @@ def update_redis_hosts(
         key_path = key_path
     )
 
-def git_status(ips, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
+def git_status(ips, key_path = KEYFILE_PATH):
+    """
+    This runs 'git status' in the InfiniCache directory of 
+    """
     print("Checking status now...")
-    command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/; git status"
+    command = "cd %s; git status" % INFINISTORE_DIRECTORY
     execute_command(command, 3, get_pty = True, ips = ips, key_path = key_path)
 
-def pull_from_github(ips, reset_first = False, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
+def pull_from_github(ips, reset_first = False, key_path = KEYFILE_PATH):
     if reset_first:
         print("Resetting first...")
-        command_reset = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; git reset --hard origin/config_ben"
+        command_reset = "cd %sevaluation; git reset --hard origin/config_ben" % INFINISTORE_DIRECTORY
         execute_command(command_reset, 2, get_pty = True, ips = ips, key_path = key_path)
     
     print("Now pulling...")
-    command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; git pull"
+    command = "cd %sevaluation; git pull" % INFINISTORE_DIRECTORY
     execute_command(command, 2, get_pty = True, ips = ips, key_path = key_path)
 
-def launch_infinistore_proxies(ips, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
-    # command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; export PATH=$PATH:/usr/local/go/bin; export GOPATH=/home/ubuntu/project; make start-server"
-    # command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; export GOPATH=/home/ubuntu/project; make start-server"
-    #command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; export PATH=$PATH:/usr/local/go/bin; export GOPATH=/home/ubuntu/project; ./server.sh >./log 2>&1 &"
+def launch_infinistore_proxies(ips, key_path = KEYFILE_PATH):
+    # command = "cd %sevaluation; export PATH=$PATH:/usr/local/go/bin; export GOPATH=/home/ubuntu/project; make start-server" % INFINISTORE_DIRECTORY
+    # command = "cd %sevaluation; export GOPATH=/home/ubuntu/project; make start-server" % INFINISTORE_DIRECTORY
+    #command = "cd %sevaluation; export PATH=$PATH:/usr/local/go/bin; export GOPATH=/home/ubuntu/project; ./server.sh >./log 2>&1 &" % INFINISTORE_DIRECTORY
     prefix = datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M') + "/"
     
     # This starts the proxies successfully, but I get failures almost immediately during workload. May be entirely unrelated.
@@ -277,20 +412,20 @@ def launch_infinistore_proxies(ips, key_path = "G:\\Documents\\School\\College\\
         ip = ips[i]
         lambda_prefix = "CacheNode%d-" % i 
         print("Assigning lambda prefix \"%s\" to proxy at ip %s." % (lambda_prefix, ip))
-        command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; export PATH=$PATH:/usr/local/go/bin; go run $PWD/../proxy/proxy.go -debug=true -prefix={} -lambda-prefix={} -disable-color >./log 2>&1".format(prefix, lambda_prefix)
+        command = "cd {}evaluation; export PATH=$PATH:/usr/local/go/bin; go run $PWD/../proxy/proxy.go -debug=true -prefix={} -lambda-prefix={} -disable-color >./log 2>&1".format(INFINISTORE_DIRECTORY, prefix, lambda_prefix)
         execute_command(command, 3, get_pty = True, ips = [ip], key_path = key_path)
 
     # This does NOT work.
-    #command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; export PATH=$PATH:/usr/local/go/bin; make start-server; cat log; cat log"
+    #command = "cd %sevaluation; export PATH=$PATH:/usr/local/go/bin; make start-server; cat log; cat log" % INFINISTORE_DIRECTORY
 
     return prefix
 
-def export_cloudwatch_logs(ips, prefix, start, end, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
-    command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation/cloudwatch; ./export_ubuntu.sh %s %s %s;" % (prefix, str(start), str(end))
+def export_cloudwatch_logs(ips, prefix, start, end, key_path = KEYFILE_PATH):
+    command = "cd %sevaluation/cloudwatch; ./export_ubuntu.sh %s %s %s;" % (INFINISTORE_DIRECTORY, prefix, str(start), str(end))
     execute_command(command, 3, get_pty = True, ips = ips, key_path = key_path)
 
-def stop_infinistore_proxies(ips, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
-    command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/evaluation; make stop-server"
+def stop_infinistore_proxies(ips, key_path = KEYFILE_PATH):
+    command = "cd %sevaluation; make stop-server" % INFINISTORE_DIRECTORY
     execute_command(command, 1, get_pty = True, ips = ips, key_path = key_path)
 
 # lc.kill_go_processes(ips = worker_ips)
@@ -298,12 +433,12 @@ def stop_infinistore_proxies(ips, key_path = "G:\\Documents\\School\\College\\Ju
 # lc.kill_go_processes(ips = [client_ip])
 def kill_go_processes(
     ips = None,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"
+    key_path = KEYFILE_PATH
 ):
     kill_command = "sudo ps aux | grep go | awk '{print $2}' | xargs kill -9 $1"
     execute_command(kill_command, 0, get_pty = True, ips = ips, key_path = key_path)
 
-def kill_proxies(ips, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
+def kill_proxies(ips, key_path = KEYFILE_PATH):
     kill_command = "sudo ps aux | grep proxy | awk '{print $2}' | xargs kill -9 $1"
     execute_command(kill_command, 0, get_pty = True, ips = ips, key_path = key_path)
 
@@ -324,7 +459,7 @@ def clear_redis_instances(
 
 # lc.clean_workers(worker_ips = worker_ips)
 def clean_workers(
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem",
+    key_path = KEYFILE_PATH,
     worker_ips = None
 ):
     #command = "cd /home/ubuntu/project/src/InfiniCacheMapReduceTest/main/;pwd;sudo rm WorkerLog*; sudo rm *.dat"
@@ -340,7 +475,7 @@ def clean_workers(
 def launch_workers(
     client_ip = None,
     count_limit = 5,
-    key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem",
+    key_path = KEYFILE_PATH,
     workers_per_vm = 5,
     worker_ips = None
 ):
@@ -366,11 +501,11 @@ def launch_workers(
         get_pty = True 
     )
 
-def update_lambdas_prefixed(ips, prefix = "CacheNode", key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
+def update_lambdas_prefixed(ips, prefix = "CacheNode", key_path = KEYFILE_PATH):
     for i in range(0, len(ips)):
         ip = ips[i]
         lambda_prefix = prefix + "{}-".format(i)
-        command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/deploy; export PATH=$PATH:/usr/local/go/bin; ./update_function.sh {} {}".format(random.randint(60, 100), lambda_prefix)
+        command = "cd %sdeploy; export PATH=$PATH:/usr/local/go/bin; ./update_function.sh {} {}".format(INFINISTORE_DIRECTORY, random.randint(60, 100), lambda_prefix)
         print("Full command: {}".format(command))
         execute_command(
             command = command,
@@ -380,8 +515,8 @@ def update_lambdas_prefixed(ips, prefix = "CacheNode", key_path = "G:\\Documents
             get_pty = True 
         )    
 
-def update_lambdas(ips, key_path = "G:\\Documents\\School\\College\\Junior Year\\CS 484_\\HW1\\CS484_Desktop.pem"):
-    command = "cd /home/ubuntu/project/src/github.com/mason-leap-lab/infinicache/deploy; export PATH=$PATH:/usr/local/go/bin; ./update_function.sh {}".format(random.randint(600, 900))
+def update_lambdas(ips, key_path = KEYFILE_PATH):
+    command = "cd %sdeploy; export PATH=$PATH:/usr/local/go/bin; ./update_function.sh {}".format(INFINISTORE_DIRECTORY, random.randint(600, 900))
     print("Full command: {}".format(command))
     execute_command(
         command = command,
@@ -510,6 +645,6 @@ if __name__ == "__main__":
 # cd ../deploy
 # ./update_function.sh 607
 
-#go run client.go -driverHostname 10.0.109.88:1234 -jobName srt -nReduce 90 -sampleDataKey sample_data.dat -dataShards 10 -parityShards 2 -maxGoRoutines 32 -storageIps 10.0.109.88:6378 -storageIps 10.0.74.253:6378 -s3KeyFile /home/ubuntu/project/src/github.com/Scusemua/InfiniCacheMapReduceTest/util/100MB_S3Keys.txt 
+#   
 #go run $PWD/../proxy/proxy.go -debug=true -prefix=202011291702 -lambda-prefix=CacheNode0- -disable-color >./log 2>&1
 #go run $PWD/../proxy/proxy.go -debug=true -prefix=202011291702 -lambda-prefix=CacheNode1- -disable-color >./log 2>&1
