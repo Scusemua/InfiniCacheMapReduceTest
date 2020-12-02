95c64fe10f75eb2d961f0db8336631313034cc68
Ben <benrcarver@gmail.com>
2020-12-02 13:09:39 -0500

    Added much more substancial documentation, including a full example, to generate_data.py.

--- main/generate_data.py
+++ main/generate_data.py
@@ -2,6 +2,98 @@ import argparse
 import sys 
 import os 
 
+# This file is a driver script to generate new data/problem sizes for MapReduce tests.
+# This assumes you have gensort installed. http://www.ordinal.com/gensort.html
+# 
+# Example Usage:
+# I want to create a 100GB problem size.
+# I create a folder called 100gb.
+#
+# I see that 10,000,000 records are required for 1GB. I need 100x this, so I need to generate
+# 1,000,000,000 records (wow!). I need to determine how many chunks/partitions I want the data
+# to be in. Let's say I want twenty partitions, where each partition is 5GB. I divide the number
+# of records to generate by 20.
+#
+# 1,000,000,000 / 20 = 50,000,000. This will be the value of the incr parameter.
+#
+# python3 generate_data.py -start 0 -end 1000000000 -incr 50000000 -file 100gb/100gb -threads 8
+#
+# You can use however many threads you want. More is usually faster, but I think this can sometimes
+# result in errors? I'm not really sure. I usually do 8-16 and it works just fine.
+#
+# This will generate twenty partitions. The first partition will contain keys 0 to 49,999,999. The
+# second will be 50,000,000 to 99,999,999, etc. The partitions generated will be put in the 100gb
+# folder that you created. Their names will be:
+#
+# 100gb-part0-0-thru-50000000.dat
+# 100gb-part1-50000000-thru-100000000.dat
+# 100gb-part2-100000000-thru-150000000.dat
+# 100gb-part3-150000000-thru-200000000.dat
+# 100gb-part4-200000000-thru-250000000.dat
+# 100gb-part5-250000000-thru-300000000.dat
+# 100gb-part6-300000000-thru-350000000.dat
+# 100gb-part7-350000000-thru-400000000.dat
+# 100gb-part8-400000000-thru-450000000.dat
+# 100gb-part9-450000000-thru-500000000.dat
+# 100gb-part10-500000000-thru-550000000.dat 
+# 100gb-part11-550000000-thru-600000000.dat
+# 100gb-part12-600000000-thru-650000000.dat
+# 100gb-part13-650000000-thru-700000000.dat
+# 100gb-part14-700000000-thru-750000000.dat
+# 100gb-part15-750000000-thru-800000000.dat
+# 100gb-part16-800000000-thru-850000000.dat
+# 100gb-part17-850000000-thru-900000000.dat
+# 100gb-part18-900000000-thru-950000000.dat
+# 100gb-part19-950000000-thru-1000000000.dat
+#
+# The reason we want to create them in their own folder is because we can use the AWS CLI to upload
+# the whole folder to S3 with a single command.
+#
+# Enter the folder (e.g., "cd 100gb") and execute the following:
+#
+# aws s3 sync . <BUCKET_URI>
+#
+# The bucket I use is infinistore-mapreduce, so I'd execute
+#
+# aws s3 sync . s3://infinistore-mapreduce
+#
+# This will automatically upload all of the partitions to S3.
+#
+# Then, you need to create a file containing the S3 keys of all this data. I generally use the
+# naming convention for these files as <SIZE>_S3Keys.txt, but you can use whatever you want in theory.
+# So I would make a file in /util called 100GB_S3Keys.txt. This script will output the filenames of all
+# the partitions generated. Since we put them into a folder, this would be:
+#
+# 100gb/100gb-part0-0-thru-50000000.dat
+# 100gb/100gb-part1-50000000-thru-100000000.dat
+# 100gb/100gb-part2-100000000-thru-150000000.dat
+# 100gb/100gb-part3-150000000-thru-200000000.dat
+# 100gb/100gb-part4-200000000-thru-250000000.dat
+# 100gb/100gb-part5-250000000-thru-300000000.dat
+# 100gb/100gb-part6-300000000-thru-350000000.dat
+# 100gb/100gb-part7-350000000-thru-400000000.dat
+# 100gb/100gb-part8-400000000-thru-450000000.dat
+# 100gb/100gb-part9-450000000-thru-500000000.dat
+# 100gb/100gb-part10-500000000-thru-550000000.dat 
+# 100gb/100gb-part11-550000000-thru-600000000.dat
+# 100gb/100gb-part12-600000000-thru-650000000.dat
+# 100gb/100gb-part13-650000000-thru-700000000.dat
+# 100gb/100gb-part14-700000000-thru-750000000.dat
+# 100gb/100gb-part15-750000000-thru-800000000.dat
+# 100gb/100gb-part16-800000000-thru-850000000.dat
+# 100gb/100gb-part17-850000000-thru-900000000.dat
+# 100gb/100gb-part18-900000000-thru-950000000.dat
+# 100gb/100gb-part19-950000000-thru-1000000000.dat
+#
+# I copy-and-paste this into the 100GB_S3Keys.txt file, then use find-and-replace to get rid of the
+# beginning. For find, I use "100gb/100gb" and for replace, I use "100gb". This turns the beginning
+# of each key from "100gb/100gb-part..." to "100gb-part...". Now you are done!
+
+
+# The following lists the number of entries required for various problem sizes.
+# Use this as a reference when determining how many entries to generate for your desired 
+# problem size. 
+
 # 100 records is 10kB.
 # 1,000 records is 100kB.
 # 10,000 records is 1MB.
