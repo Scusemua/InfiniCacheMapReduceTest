a91a84cc3316d6d6d471e049405c77ea46efdca0
Ben <benrcarver@gmail.com>
2020-12-03 18:53:49 -0500

    Added prints to build.sh

--- plugins/build.sh
+++ plugins/build.sh
@@ -1,8 +1,11 @@
+echo "Building TeraSort now."
 go build --buildmode=plugin -o srtr_service.so srtr_service.go
 go build --buildmode=plugin -o srtm_service.so srtm_service.go
 
+echo "Building Word Count now."
 go build --buildmode=plugin -o wcm_service.so wcm_service.go
 go build --buildmode=plugin -o wcr_service.so wcr_service.go
 
+echo "Building grep now."
 go build --buildmode=plugin -o grepr_service.so grepr_service.go
 go build --buildmode=plugin -o grepm_service.so grepm_service.go
